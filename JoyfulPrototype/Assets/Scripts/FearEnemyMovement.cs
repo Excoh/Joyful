329513da7e5bf3356eb3315303ca6d0436e61ea6
Eric Tang <Erictangis@gmail.com>
2016-02-12 04:39:00 -0700

    New Fear Prototype

--- JoyfulPrototype/Assets/Scripts/FearEnemyMovement.cs
+++ JoyfulPrototype/Assets/Scripts/FearEnemyMovement.cs
@@ -3,90 +3,207 @@ using System.Collections;
 
 public class FearEnemyMovement : MonoBehaviour {
 
-	public float moveSpeed;
-	public bool moveRight;
+    //Psuedo-Constants
+    Vector3 LEFT_SPRITE = new Vector3(1f, 1f, 1f);
+    Vector3 RIGHT_SPRITE = new Vector3(-1f, 1f, 1f);
+
+    //Public Members
+    public float moveSpeed;
 	public float jumpHeight;
-	
-	public Transform wallCheck;
+    public float jumpDistance;
 	public float wallCheckRadius;
-	public LayerMask whatIsWall;
-	private bool hittingWall;
-	
-	public Transform groundCheck;
-	public float groundCheckRadius;
-	public LayerMask whatIsGround;
-	private bool grounded;
-	
-	public float playerCheckRadius;
-	public LayerMask whatIsPlayer;
-	public bool playerFound;
-	
-	private bool notAtEdge;
-	public Transform edgeCheck;
+    public float groundCheckRadius;
+    public float playerCheckRadius;
+    public float jumpCooldownTime;
+    public LayerMask whatIsWall; //Lefted as public because Wall may have its own Layer Mask.
+    public float timeAllowedToBeAwake;
 
-	private SpriteRenderer spriteOn;
+    //Private Members
+    private Vector2 _startPosition;
+	private SpriteRenderer _spriteOn;
+    private Rigidbody2D _rigidbody;
+    private Transform _wallCheck;
+    private Transform _groundCheck;
+    private Transform _edgeCheck;
+    private Transform _playerTransform;
+    private bool _atEdge;
+    private bool _grounded;
+    private bool _hittingWall;
+    private bool _playerFound;
+    private LayerMask _groundLayerMask;
+    private bool _movingLeft;
+    private bool _movingRight;
+    private bool _jumping;
+    private bool _returning;
+    private bool _hiding;
+    private float _timeOfJump;
+    private float _timeWokenUp;
+    private float _distanceFromPlayer;
 
-	public Transform playerTransform;
 
-	void Start () 
+    void Start () 
 	{
-		spriteOn = GetComponent<SpriteRenderer> ();
-		spriteOn.enabled = false; //Enemy Hidden and Idle
-	}
-
-	void FixedUpdate()
-	{this.grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);}
+        _Init_Fear();
+        _spriteOn.enabled = false; //Enemy Hidden and Idle
+    }
+    
+    private void _Init_Fear()
+    {
+        try {
+            _startPosition = this.gameObject.transform.position;
+            _spriteOn = this.gameObject.GetComponent<SpriteRenderer>();
+            _rigidbody = this.gameObject.GetComponent<Rigidbody2D>();
+            _wallCheck = this.gameObject.transform.FindChild("WallCheck");
+            _groundCheck = this.gameObject.transform.FindChild("GroundCheck");
+            _edgeCheck = this.gameObject.transform.FindChild("EdgeCheck");
+            _playerTransform = GameObject.FindWithTag("Player").transform;
+            _groundLayerMask = LayerMask.GetMask("Ground");
+            _movingLeft = true;
+            _movingRight = false;
+            _hiding = true;
+            _returning = false;
+        }
+        catch{
+            Debug.Log("Something is wrong with _Init_Fear()");
+        }
+    }
 
 	void Update () 
 	{
-		playerFound = Physics2D.OverlapCircle (transform.position, playerCheckRadius, whatIsPlayer);
-
-		if (playerFound) //If player is within range
-		{
-			spriteOn.enabled = true;
-			AttackPlayer();
-		}
-
-		else //Return to being idle
-		{spriteOn.enabled = false;}
+        _Sense();
+        _Think();
+        _Act();
 	}
 
-	void AttackPlayer () //When Player is in range, Fear will chase Player
+    private void _Sense()
+    {
+        this._grounded = Physics2D.OverlapCircle(_groundCheck.position, groundCheckRadius, _groundLayerMask);
+        this._distanceFromPlayer = Vector2.Distance(_playerTransform.position, transform.position);
+
+        //If enemy returned to original location
+        if (_returning && Vector2.Distance(transform.position,_startPosition) < 0.8f)
+        {
+            _hiding = true;
+            _returning = false;
+            _spriteOn.enabled = false; //Return to being idle and invisible.
+        }
+    }
+
+    private void _Think()
+    {
+        //If awake for set time, go back to original position.
+        if (!_hiding && _timeWokenUp + timeAllowedToBeAwake < Time.time)
+        {
+            _returning = true;
+        }
+        else
+        {
+            if (_distanceFromPlayer < playerCheckRadius) //If player is within range.
+            {
+                if (_hiding)
+                {
+                    _hiding = false;
+                    _timeWokenUp = Time.time;
+                    _spriteOn.enabled = true;
+                }
+            }
+
+            //Checks if the enemy can jump again.
+            if (_timeOfJump + jumpCooldownTime < Time.time)
+            {
+                _jumping = false;
+            }
+        }
+    }
+
+    private void _Act()
+    {
+        //Time limit has been reached. Enemy must go back to sleep.
+        if (_returning)
+        {
+            _ReturnToStartPosition();
+        }
+        else
+        {
+            if (_distanceFromPlayer < playerCheckRadius && _rigidbody.velocity.y == 0 && _distanceFromPlayer > 1f) //If player is within range.
+            {
+                _Move();
+                AttackPlayer();       
+            }
+        }
+    }
+
+    private void _Move()
+    {
+        float relativePoint = _playerTransform.position.x - transform.position.x;
+
+        //Moving Left
+        if (relativePoint < 0.0 )
+        {
+            transform.localScale = LEFT_SPRITE;
+            _rigidbody.velocity = new Vector2(-moveSpeed, _rigidbody.velocity.y);
+            _movingLeft = true;
+            _movingRight = false;
+        }
+        //Moving Right
+        else if (relativePoint >= 0.0 )
+        {
+            transform.localScale = RIGHT_SPRITE;
+            _rigidbody.velocity = new Vector2(moveSpeed, _rigidbody.velocity.y); ;
+            _movingRight = true;
+            _movingLeft = false;
+        }     
+    }
+
+    private void _ReturnToStartPosition()
+    {
+        float relativePoint = _startPosition.x - transform.position.x;
+
+        //Moving Left
+        if (relativePoint < 0.0)
+        {
+            transform.localScale = LEFT_SPRITE;
+            _rigidbody.velocity = new Vector2(-moveSpeed, _rigidbody.velocity.y);
+            _movingLeft = true;
+            _movingRight = false;
+        }
+        //Moving Right
+        else if (relativePoint >= 0.0)
+        {
+            transform.localScale = RIGHT_SPRITE;
+            _rigidbody.velocity = new Vector2(moveSpeed, _rigidbody.velocity.y); ;
+            _movingRight = true;
+            _movingLeft = false;
+        }
+    }
+
+    void AttackPlayer () //When Player is in range, Fear will chase Player
 	{
-		this.hittingWall = Physics2D.OverlapArea (wallCheck.position, groundCheck.position, whatIsWall);
-		
-		notAtEdge = Physics2D.OverlapCircle (edgeCheck.position, wallCheckRadius, whatIsWall);
+        if (!_jumping)
+        {
+            this._hittingWall = Physics2D.OverlapArea(_wallCheck.position, _groundCheck.position, whatIsWall);
 
-		if (!notAtEdge && this.grounded)
-		{Jump ();}
-		
-		if (hittingWall && this.grounded) 
-		{Jump ();}
+            this._atEdge = Physics2D.OverlapCircle(_edgeCheck.position, wallCheckRadius, whatIsWall);
 
-		//Vector2 relativePoint = transform.InverseTransformPoint(playerTransform.position);
-
-		float relativePoint = playerTransform.position.x - transform.position.x;
-
-		if (relativePoint > 0.0)
-		{
-			transform.localScale = new Vector3(-1f, 1f, 1f);
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
-
-			print ("Object is to the left");
-		}
-
-		else if (relativePoint < 0.0)
-		{
-			transform.localScale = new Vector3(1f, 1f, 1f);
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
-
-			print ("Object is to the right");
-		}
-
-		else 
-		{print ("conflict");}
+            if (this._atEdge && this._grounded || this._hittingWall && this._grounded)
+            {
+                _timeOfJump = Time.time;
+                Jump();
+                print(_rigidbody.velocity);
+                _jumping = true;
+            }
+        }
 	}
 
 	void Jump()
-	{GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);}
+	{
+        if (_movingLeft)
+        {
+            _rigidbody.AddForce(new Vector2(-jumpDistance, jumpHeight), ForceMode2D.Force);
+        }
+        else if (_movingRight)
+        {
+            _rigidbody.AddForce(new Vector2(jumpDistance, jumpHeight), ForceMode2D.Force);
+        }
+    }
 }
